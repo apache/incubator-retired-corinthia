3ea65efa58549e9ac0b78ea24174302d6eb32b2e
Peter Kelly <peter@uxproductivity.com>
2014-08-14 19:43:00 +0700

    Change license to ASLv2

--- DocFormats/lib/DFZipFile.h
+++ DocFormats/lib/DFZipFile.h
@@ -1,10 +1,16 @@
+// Copyright 2012-2014 UX Productivity Pty Ltd
 //
-//  DFZipFile.h
-//  DocFormats
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Peter Kelly on 26/12/11.
-//  Copyright (c) 2011-2014 UX Productivity Pty Ltd. All rights reserved.
+// http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef DocFormats_DFZipFile_h
 #define DocFormats_DFZipFile_h
