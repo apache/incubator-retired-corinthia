748d707808170795dd82caae4d4dd5c89cf1eb1b
franzdecopenhague <franzdecopenhague@outlook.com>
2015-03-16 12:35:01 +0000

    Added put convert to dfwebserver python binding

--- consumers/dfwebserver/test.py
+++ consumers/dfwebserver/test.py
@@ -1,5 +1,29 @@
+# To test:
+#
+# make clean dev
+# python test.py
+
+import unittest
+
 import dfutil
 import dfconvert
+import shutil
 
-print dfutil.normalize("other.html")
-print dfconvert.get("input.docx", "output.html");
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        
+        self.assertTrue(dfutil.normalize("other.html"))
+
+        self.assertTrue(dfconvert.get("input.docx", "output.html"))
+        
+        shutil.copyfile("input.docx", "dummy.docx");
+        
+        self.assertTrue(dfconvert.put("dummy.docx", "output.html"))
+        
+        self.assertTrue(dfconvert.create("output.docx", "output.html"))
+        
+        
+if __name__ == '__main__':
+    unittest.main()        
+
