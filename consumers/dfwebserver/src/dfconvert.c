9350a3383845ba30ef0863c4dc61ccbd6273f95e
Franz de Copenhague <franzdecopenhague@outlook.com>
2015-03-16 22:44:40 +0700

    Added put convert to dfwebserver python binding

--- consumers/dfwebserver/src/dfconvert.c
+++ consumers/dfwebserver/src/dfconvert.c
@@ -4,9 +4,12 @@ typedef struct DFError DFError;
 void DFErrorRelease(DFError *error);
 const char *DFErrorMessage(DFError **error);
 
-int DFGetFile(const char *concreteFilename,
-              const char *abstractFilename,
-              DFError **error);
+// Abstraction level 1
+
+int DFGetFile(const char *concrete, const char *abstract, DFError **error);
+int DFPutFile(const char *concrete, const char *abstract, DFError **error);
+int DFCreateFile(const char *concrete, const char *abstract, DFError **error);
+              
 
 static PyObject* get_func(PyObject* self, PyObject* args)
 {
@@ -28,10 +31,53 @@ static PyObject* get_func(PyObject* self, PyObject* args)
     Py_RETURN_FALSE;
 }
 
+static PyObject* put_func(PyObject* self, PyObject* args)
+{
+    DFError *error = NULL;
+    
+    char * concrete = NULL;
+    char * abstract = NULL;
+    
+    if (!PyArg_ParseTuple(args, "ss", &concrete, &abstract)) {
+        return NULL;
+    }    
+    
+    if (DFPutFile(concrete, abstract, &error)) {
+        Py_RETURN_TRUE;
+    }
+
+    fprintf(stderr,"%s\n",DFErrorMessage(&error));
+    DFErrorRelease(error);
+    Py_RETURN_FALSE;
+}
+
+static PyObject* create_func(PyObject* self, PyObject* args)
+{
+    DFError *error = NULL;
+    
+    char * concrete = NULL;
+    char * abstract = NULL;
+    
+    if (!PyArg_ParseTuple(args, "ss", &concrete, &abstract)) {
+        return NULL;
+    }    
+    
+    if (DFCreateFile(concrete, abstract, &error)) {
+        Py_RETURN_TRUE;
+    }
+
+    fprintf(stderr,"%s\n",DFErrorMessage(&error));
+    DFErrorRelease(error);
+    Py_RETURN_FALSE;
+}
+
+
 /*  define functions in module */
 static PyMethodDef dfconvertMethods[] =
 {
      {"get", get_func, METH_VARARGS, "Create a new HTML file from input document"},
+     {"put", put_func, METH_VARARGS, "Update an existing Word document based on a modified HTML file."},
+     {"create", create_func, METH_VARARGS, "Create a new Word document from a HTML file. The Word document must not already exist."},
      {NULL, NULL, 0, NULL}
 };
 
